977968e7c0c6426797475c0ad418e964ff5aced3
qqqqxxx <tao_qingxiao@sjtu.edu.cn>
2021-04-19 01:44:18 +0800

    finish skiplist, don't forget to modify put

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 LINK.o = $(LINK.cc)
-CXXFLAGS = -std=c++14 -Wall
+CXXFLAGS = -std=c++14 -Wall -g
 
 all: correctness persistence
 
--- kvstore.cc
+++ kvstore.cc
@@ -1,12 +1,129 @@
 #include "kvstore.h"
 #include <string>
 
+Skiplist::Skiplist() {
+	siz = 0;
+	head = new Node;
+}
+
+Skiplist::~Skiplist() {
+	Node *headPtr = head;
+    while(headPtr) {
+        Node *ptr = headPtr;
+        headPtr = headPtr->down;
+        while(ptr) {
+            Node *tmp = ptr;
+            ptr = ptr->right;
+            delete tmp;
+        }
+    }
+}
+
+size_t Skiplist::size() {
+	return siz;
+}
+
+void Skiplist::clear() {
+	Node *headPtr = head;
+    while(headPtr) {
+        Node *ptr = headPtr;
+        headPtr = headPtr->down;
+        while(ptr) {
+            Node *tmp = ptr;
+            ptr = ptr->right;
+            delete tmp;
+        }
+    }
+    siz = 0;
+    head = new Node;
+}
+
+std::string* Skiplist::get(const uint64_t& key) {
+	Node *p = head;
+    while(p) {
+        while(p->right && p->right->key < key) {
+            p = p->right;
+        }
+        if(p->right && p->right->key == key) {
+            p = p->right;
+            std::string* res = &(p->val);
+            return res;
+        } else {
+            p = p->down;
+        }
+    }
+    return nullptr;
+}
+
+void Skiplist::put(const uint64_t& key, const std::string& val) {
+    ++ siz;
+    std::vector<Node*> pathList;    //从上至下记录搜索路径
+    Node *p = head;
+    while(p){
+        while(p->right && p->right->key < key){ 
+            p = p->right;
+        }
+        pathList.push_back(p);
+        p = p->down;
+    }
+    bool insertUp = true;
+    Node* downNode= nullptr;
+    while(insertUp && pathList.size() > 0){   //从下至上搜索路径回溯，50%概率
+        Node *insert = pathList.back();
+        pathList.pop_back();
+        insert->right = new Node(insert->right, downNode, key, val); //add新结点
+        downNode = insert->right;    //把新结点赋值为downNode
+        insertUp = (rand()&1);   //50%概率
+    }
+    if(insertUp){  //插入新的头结点，加层
+        Node * oldHead = head;
+        head = new Node;
+        head->right = new Node(NULL, downNode, key, val);
+        head->down = oldHead;
+    }
+}
+
+bool Skiplist::remove(const uint64_t& key) {
+    Node *p = head;
+    Node *prev = nullptr;
+    while(p) {
+        while(p->right && p->right->key < key) {
+            p = p->right;
+        }
+        if(p->right && p->right->key == key) {
+            prev = p;
+            break;
+        } else {
+            p = p->down;
+        }
+    }
+    if(prev == nullptr) {
+        return false;
+    } else {
+        -- siz;
+        std::string valtmp = prev->val;
+        while(prev) {
+            Node *tmp = prev->right;
+            Node *toberemove = tmp->down;
+            prev->right = tmp->right;
+            delete tmp;
+            prev = prev->down;
+            while(prev && prev->right && prev->right != toberemove) {
+                prev = prev->right;
+            }
+        }
+        return true;
+    }
+}
+
 KVStore::KVStore(const std::string &dir): KVStoreAPI(dir)
 {
+	MemTable = new Skiplist;
 }
 
 KVStore::~KVStore()
 {
+	delete MemTable;
 }
 
 /**
@@ -15,6 +132,7 @@ KVStore::~KVStore()
  */
 void KVStore::put(uint64_t key, const std::string &s)
 {
+	MemTable->put(key, s);
 }
 /**
  * Returns the (string) value of the given key.
@@ -22,7 +140,9 @@ void KVStore::put(uint64_t key, const std::string &s)
  */
 std::string KVStore::get(uint64_t key)
 {
-	return "";
+	std::string *tmp = MemTable->get(key);
+	if(tmp) return *tmp;
+    else return "";
 }
 /**
  * Delete the given key-value pair if it exists.
@@ -30,7 +150,7 @@ std::string KVStore::get(uint64_t key)
  */
 bool KVStore::del(uint64_t key)
 {
-	return false;
+	return MemTable->remove(key);
 }
 
 /**
@@ -39,4 +159,5 @@ bool KVStore::del(uint64_t key)
  */
 void KVStore::reset()
 {
+	MemTable->clear();
 }
