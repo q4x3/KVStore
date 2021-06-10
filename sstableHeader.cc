51190a9487d32f26046de4638f6b8d1b85fbc079
qqqqxxx <tao_qingxiao@sjtu.edu.cn>
2021-06-10 22:44:46 +0800

    ..

--- sstableHeader.cc
+++ sstableHeader.cc
@@ -6,19 +6,11 @@ sstableHeader::sstableHeader(uint64_t _t, uint64_t _cnt, uint64_t _max, uint64_t
 void sstableHeader::read(const std::string& file, uint32_t startLoc) {
     std::ifstream f;
     f.open(file, std::ios_base::in | std::ios_base::binary);
-    if(!f.is_open()) {
-        std::cout << "Fail to open file in path: " + file << std::endl;
-        exit(1);
-    }
     f.seekg(startLoc, std::ios_base::beg);
     f.read((char*)&timeStamp, sizeof(uint64_t));
     f.read((char*)&keyCounter, sizeof(uint64_t));
     f.read((char*)&maxKey, sizeof(uint64_t));
     f.read((char*)&minKey, sizeof(uint64_t));
-    if(!f.good()) {
-        std::cout << "Fail to read at location: " + std::to_string(startLoc) << std::endl;
-        exit(1);
-    }
     f.close();
     return;
 }
@@ -26,18 +18,10 @@ void sstableHeader::read(const std::string& file, uint32_t startLoc) {
 void sstableHeader::write(const std::string& file) const {
     std::ofstream f;
     f.open(file, std::ios_base::out | std::ios_base::binary | std::ios_base::app);
-    if(!f.is_open()) {
-        std::cout << "Fail to open file in path: " + file << std::endl;
-        exit(1);
-    }
     f.write((char*)&timeStamp, sizeof(uint64_t));
     f.write((char*)&keyCounter, sizeof(uint64_t));
     f.write((char*)&maxKey, sizeof(uint64_t));
     f.write((char*)&minKey, sizeof(uint64_t));
-    if(!f.good()) {
-        std::cout << "Fail to write in file: " + file << std::endl;
-        exit(1);
-    }
     f.close();
     return;
 }
